d5df63d2a8a3d581c21ebf1bae067f78ad2819b2
Mattia Righetti <matt95.righetti@gmail.com>
2020-12-27 01:18:45 +0100

    Added custom omz aliases

---
+++ omz/.oh-my-zsh/custom/aliases/miscellaneous.zsh
@@ -0,0 +1,8 @@
+# Trim new lines and copy to clipboard
+alias c="tr -d '\n' | pbcopy"
+
+# Recursively delete `.DS_Store` files
+alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
+
+# Print each PATH entry on a separate line
+alias path='echo -e ${PATH//:/\\n}'
